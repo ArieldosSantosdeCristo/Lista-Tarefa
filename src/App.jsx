e3b6000403626a2c454a0f3a1952e1309cbafaf3
Ariel dos Santos de Cristo Góes <santosng456@gmail.com>
2025-03-26 20:01:17 -0300

    Adcionando edição dos arquivos, search, filter.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import { useState } from 'react';
 import Todo from "./components/Todo";
-import "./App.css";
 import Todoform from './components/Todoform';
+import Search from './components/Search';
+import Filter from './components/Filter';
+
+
+import "./App.css";
+
 
 
 function App() {
@@ -32,6 +37,8 @@ const [todos,setTodos] = useState([
   },
 ]);
 
+const [search, setSearch] = useState("");
+
 const addTodo= (text, category) => {
   const newtodos = [
     ...todos, 
@@ -41,16 +48,35 @@ const addTodo= (text, category) => {
     isCompleted: false,
   },
 ];
-setTodos(newtodos)
-}
+setTodos(newtodos);
+};
+
+const removeTodo = (id) => {
+  const newTodos = [...todos]
+  const filteredTodos = newTodos.filter 
+  ((todo) => todo.id !==id ? todo:null);
+
+  setTodos (filteredTodos);
+};
+
+const completeTodo = (id) => {
+  const newTodos = [...todos]
+  newTodos.map((todo) => todo.id === id ? todo.isCompleted = !todo.isCompleted : todo
+);
+setTodos(newTodos);
+};
 
   return( 
   <div className="app">
       <h1>Lista de Tarefas</h1>
+      <Search search={search} setSearch={setSearch}/>
+      <Filter/>
   <div className='todo-list'>
 
-    {todos.map((todo) =>(
-      <Todo key={todo.id} todo={todo} />
+    {todos.filter((todo) => todo.text.toLowerCase().includes(search.toLowerCase())
+    )
+    .map((todo) =>(
+      <Todo key={todo.id} todo={todo}  removeTodo={removeTodo} completeTodo={completeTodo}/>
     ))}
 
   </div>
